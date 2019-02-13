89cacda4f3e2e7842325ff9dd962bbed90313e11
Tianyu Gao <gaotianyu1350@126.com>
2019-02-13 05:30:52 +0000

    euclidean proto snowball

--- test_proto.py
+++ test_proto.py
@@ -13,10 +13,15 @@ distant = JSONFileDataLoader('./data/distant.json', './data/glove.6B.50d.json', 
 
 framework = nrekit.framework.Framework(train_data_loader, val_data_loader, test_data_loader, distant)
 sentence_encoder = nrekit.sentence_encoder.CNNSentenceEncoder(train_data_loader.word_vec_mat, max_length)
-model = models.proto.Proto(sentence_encoder, base_class=train_data_loader.rel_tot, siamese_model=None, hidden_size=230)
+sentence_encoder2 = nrekit.sentence_encoder.CNNSentenceEncoder(train_data_loader.word_vec_mat, max_length)
+model2 = models.siamese.Siamese(sentence_encoder2, hidden_size=230)
+model = models.proto.Proto(sentence_encoder, base_class=train_data_loader.rel_tot, siamese_model=model2, hidden_size=230)
 
 # load pretrain
 checkpoint = torch.load('./checkpoint/proto_encoder.pth.tar')['state_dict']
+checkpoint2 = torch.load('./checkpoint/cnn_siamese.pth.tar')['state_dict']
+for key in checkpoint2:
+    checkpoint['siamese_model.' + key] = checkpoint2[key]
 model.load_state_dict(checkpoint)
 model.cuda()
 model_name = 'proto'
@@ -25,4 +30,5 @@ model_name = 'proto'
 # framework.train(model, model_name, model2=model2)
 # framework.eval_siamese(model2, threshold=0.99)
 # print('')
-framework.eval(model, threshold=0.5, threshold_for_snowball=0.99)
+model.get_protos(train_data_loader)
+framework.eval(model, threshold=0, threshold_for_snowball=0.99)
